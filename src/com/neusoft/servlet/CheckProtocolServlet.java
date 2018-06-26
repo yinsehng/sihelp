865f89e762f13037799d2ec36029bfb671bf3522
yinsehng <1037935400@qq.com>
2018-06-26 23:15:16 +0800

    Update CheckProtocolServlet.java

--- src/com/neusoft/servlet/CheckProtocolServlet.java
+++ src/com/neusoft/servlet/CheckProtocolServlet.java
@@ -50,7 +50,7 @@ public class CheckProtocolServlet extends HttpServlet {
 		}
 		String admin = request.getParameter("ADMIN");
 		String AKB020 = request.getParameter("AKB020");
-		String akb021 = "";
+		String akbssss021 = "ajdfjsdfjsd";
 		String WQ_YEAR = request.getParameter("WQ_YEAR");
 		if (!"1".equals(admin)) {
 			//Ϊ�û���ӡ
